b87524b2f16e8dd0feb818106b4743ec272eaa84
sudip paladhi <sudippaladhi@gmail.com>
2024-01-18 02:36:26 +0530

    design updated

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -66,7 +66,7 @@ const Experience = () => {
       name="experience"
       className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-4 md:pt-0 pt-8 flex flex-col justify-center w-full h-full text-white">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
