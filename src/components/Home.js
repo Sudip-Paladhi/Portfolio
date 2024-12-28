f3e2a45235ba171de9a938302bc1f0e12ff0656c
SUDIP PALADHI <99830636+Sudip-Paladhi@users.noreply.github.com>
2024-12-28 23:36:50 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:pt-20  px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Frontend Developer
+            I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I am a skilled and passionate web developer with experience in
